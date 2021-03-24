ea990c3549055a4485d8d97ae61f00497dd6057b
Stijn Legerstee <stijnlegerstee@outlook.com>
2021-03-24 12:44:17 +0100

    fixes

--- 004 - Javascript Functies/Opdracht 4.3/js/index.js
+++ 004 - Javascript Functies/Opdracht 4.3/js/index.js
@@ -2,27 +2,27 @@ var plaatjes = [1,2,3];
 var teller = 0;
 
 var hoofd = document.getElementById("hoofd");
-hoofd.style.backgroundImage = "url('../img/hoofd1.png')";
+hoofd.style.backgroundImage = "url('img/hoofd1.png')";
 
 hoofd.addEventListener("click", function() {
     getNumber();
-    hoofd.style.backgroundImage = "url('../img/hoofd" + getNumber() +".png')";
+    hoofd.style.backgroundImage = "url('img/hoofd" + getNumber() +".png')";
 });
 
 var gezicht = document.getElementById("gezicht");
-gezicht.style.backgroundImage = "url('../img/gezicht1.png')";
+gezicht.style.backgroundImage = "url('img/gezicht1.png')";
 
 gezicht.addEventListener("click", function() {
     getNumber();
-    gezicht.style.backgroundImage = "url('../img/gezicht" + getNumber() +".png')";
+    gezicht.style.backgroundImage = "url('img/gezicht" + getNumber() +".png')";
 });
 
 var mond = document.getElementById("mond");
-mond.style.backgroundImage = "url('../img/mond1.png')";
+mond.style.backgroundImage = "url('img/mond1.png')";
 
 mond.addEventListener("click", function() {
     getNumber();
-    mond.style.backgroundImage = "url('../img/mond" + getNumber() +".png')";
+    mond.style.backgroundImage = "url('img/mond" + getNumber() +".png')";
 });
 
 function getNumber() {
