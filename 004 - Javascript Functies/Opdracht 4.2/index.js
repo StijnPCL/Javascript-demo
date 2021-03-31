1d14a96274d9fcaaff195da85f39ead75bed5a4b
Stijn Legerstee <stijnlegerstee@outlook.com>
2021-03-31 21:24:07 +0200

    final fix

--- 004 - Javascript Functies/Opdracht 4.2/index.js
+++
@@ -1,3 +0,0 @@
-var plaatjes = [1,2,3,4,5,6,7,8,9];
-var slideholder = document.getElementById("slideholder");
-document.slideholder.style.backgroundImage = "url('../img/aap1.jpg')";
--- 004 - Javascript Functies/Opdracht 4.2/js/index.js
+++ 004 - Javascript Functies/Opdracht 4.2/js/index.js
@@ -1,11 +1,11 @@
 var plaatjes = [1,2,3,4,5,6,7,8,9];
 var teller = 0;
 var slideholder = document.getElementById("slideholder");
-slideholder.style.backgroundImage = "url('../img/aap1.jpg')";
+slideholder.style.backgroundImage = "url('img/aap1.jpg')";
 
 slideholder.addEventListener("click", function() {
     getAap();
-    slideholder.style.backgroundImage = "url('../img/aap" + getAap() +".jpg')";
+    slideholder.style.backgroundImage = "url('img/aap" + getAap() +".jpg')";
 });
 
 function getAap() {
